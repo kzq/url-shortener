d8036db1578520a7771ec1485badb5be67b7e89b
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-04 22:13:24 +0000

    Send request to api

--- app/assets/javascripts/angular/controllers/shortenerCtrl.js
+++ app/assets/javascripts/angular/controllers/shortenerCtrl.js
@@ -1,4 +1,5 @@
-app.controller("shortenerCtrl",  ['$scope', function($scope) {
+app.controller('shortenerCtrl', ['$scope', 'urlShortenerFactory', function($scope, urlShortenerFactory) {
+  
   $scope.webAddresses = [
     { url: 'http://www.google.co.uk', shortForm: 'localhost:300/12lk'},
     { url: 'http://www.google.co.uk', shortForm: 'localhost:300/12lk'},
@@ -8,6 +9,13 @@ app.controller("shortenerCtrl",  ['$scope', function($scope) {
   ];
   
   $scope.addUrl = function() {
-    
+    urlShortenerFactory.save($scope.webAddressUrl,
+      function(response, _headers) {
+        $scope.webAddresses.push(response);
+      },
+      function(response) {
+        alert('Errors: ' + response.data.errors.join('. '));
+      }
+    );
   };
 }]);
