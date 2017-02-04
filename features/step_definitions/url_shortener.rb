c417c58ab228c2023cf2c381910bf9bb5c63089d
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-04 12:57:52 +0000

    Add step definitions
    Step definitions for url_shortenet.feature

---
+++ features/step_definitions/url_shortener.rb
@@ -0,0 +1,19 @@
+Given(/^I am on the home page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I provide a website address$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see the shortened version of the given address$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I click on a short URL$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should be redirected to the original addrress$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
