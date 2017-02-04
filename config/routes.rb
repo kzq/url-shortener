991c642a93c456ee5c6ba74bbf4e38be214af337
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-04 22:17:00 +0000

    Accept route for shortened_urls

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'home_page#index'
-
+   
+   namespace :api do
+    resources :shortened_urls, only: [:index] do
+    end
+   end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
