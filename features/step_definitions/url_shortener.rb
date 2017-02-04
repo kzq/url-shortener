afe501a3c4d46016cb10d170ee99a11449734e5f
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-04 14:15:12 +0000

    Add step definitions for new steps

--- features/step_definitions/url_shortener.rb
+++ features/step_definitions/url_shortener.rb
@@ -1,19 +1,23 @@
 Given(/^I am on the home page$/) do
+  visit root_path
+end
+
+When(/^I provide a website (.*)$/) do |address|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I provide a website address$/) do
+Then(/^It should display the shortened version of the given (.*)$/) do |address|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then(/^I should see the shortened version of the given address$/) do
+Given(/^I have a short URl of the (.*)$/) do |address|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I click on a short URL$/) do
+When(/^I click on a short (.*)$/) do |url|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then(/^I should be redirected to the original addrress$/) do
+Then(/^I should be redirected to the original (.*) address$/) do |website_address|
   pending # Write code here that turns the phrase above into concrete actions
 end
