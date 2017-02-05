3cd59ccfc397328df0b982c9987e1abc059c0c3e
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-05 15:32:58 +0000

    Make sure presence of url
    Add valoidations for URL and unique key

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,2 +1,6 @@
 class ShortenedUrl < ActiveRecord::Base
+  validates_presence_of :url, allow_blank: false, message: 'Please provide a URL'
+  validates_uniqueness_of :url, case_sensitive: false, message: 'Please choose another URL'
+  validates_uniqueness_of :unique_key
+
 end
