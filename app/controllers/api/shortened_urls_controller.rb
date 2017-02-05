1e958cc45e19b026400b38500416a63631e4bdcf
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-05 11:25:03 +0000

    Display short url on page
    User click add url, generated short key is displayed on page

--- app/controllers/api/shortened_urls_controller.rb
+++ app/controllers/api/shortened_urls_controller.rb
@@ -2,6 +2,25 @@ class Api::ShortenedUrlsController < ApplicationController
   respond_to :json
   
   def index
-    respond_with ShortenedUrl.all   
+    respond_with ShortenedUrl.select("id,url,unique_key")   
   end
+  
+  def create
+    url = shortened_url_params[:url]
+    #shortened_url = ShortenedUrl.new(url)
+    #shortened_url.generate
+    shortened_url = Shortener::ShortenedUrl.generate(shortened_url_params[:url])
+    if shortened_url.save
+      respond_with :api, shortened_url, status: :ok, location: api_shortened_urls_url
+    else
+      render json: { errors: shortened_url.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+ 
+  private
+  
+  def shortened_url_params
+    params.require(:shortened_url).permit(:url)
+  end
+  
 end
--- features/step_definitions/url_shortener.rb
+++ features/step_definitions/url_shortener.rb
@@ -6,10 +6,12 @@ When(/^I provide a website address (.*)$/) do |url|
   fill_in 'url', with: url
   click_button('Add')
   #page.save_screenshot('screen.png', full: true)
+  expect(page).to have_content(url)
 end
 
-Then(/^It should display the shortened version of the given (.*)$/) do |address|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^It should display the shortened version of (.*)$/) do |address|
+  shortened_url = ShortenedUrl.select("id,url,unique_key").find_by(url: "#{address}/")
+  expect(page).to have_content(shortened_url.unique_key)
 end
 
 Given(/^I have a short URl of the (.*)$/) do |address|
