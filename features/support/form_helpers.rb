484fb7dafbc8d3bfcdb3ee6c945cc8c116d182ae
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-05 21:40:35 +0000

    Submit form
    Helper to click add button and fill with url

--- features/support/form_helpers.rb
+++ features/support/form_helpers.rb
@@ -1,6 +1,13 @@
 module FormHelpers
-  def submit_form(btnId)
-    find('#'+btnId).click
+  def add_address(url)
+    fill_in 'url', with: url
+    click_button('Add')
+    expect(page).to have_content(url)
   end
+  
+  def find_short_url(url)
+    find('tr', text: url).all('td')[1]  
+  end
+  
 end
 World(FormHelpers);
