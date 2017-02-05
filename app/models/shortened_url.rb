7d8c0ff79827f7b14a885c171bbdbf245b54d8af
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-05 17:34:11 +0000

    Generate unique key for the given url

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -2,5 +2,9 @@ class ShortenedUrl < ActiveRecord::Base
   validates_presence_of :url, allow_blank: false, message: 'Please provide a URL'
   validates_uniqueness_of :url, case_sensitive: false, message: 'Please choose another URL'
   validates_uniqueness_of :unique_key
-
+  
+  def self.generate_key(url)
+    Shortener::ShortenedUrl.generate(url)  
+  end
+  
 end
