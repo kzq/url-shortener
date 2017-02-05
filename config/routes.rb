2a9c374d82a46e8392b075245a3a053ab5a788f1
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-05 21:48:53 +0000

    Add routes for URl redirection

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'home_page#index'
-   
+   get '/:id' => "shortener/shortened_urls#show"
    namespace :api do
     resources :shortened_urls, only: [:index, :create] do
     end
