f740e8ffae2469c017c912c4d58b89654166622c
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-05 12:04:57 +0000

    Add example group for rspec

--- spec/models/shortened_url_spec.rb
+++ spec/models/shortened_url_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ShortenedUrl, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates the presence of url" do
+    
+  end
+  
+  it "prepends http to the url" do
+    
+  end
+  
+  it "generates unique key of the given url" do
+      
+  end
+  
+  it "returns short form of the urll" do
+    
+  end
 end
