1f18a995177e48f7d5539ac33fba0911e690e591
Kamran Zafar <kami_ravian@yahoo.com>
2017-02-05 15:35:11 +0000

    Should test validations
    Test validations

--- spec/models/shortened_url_spec.rb
+++ spec/models/shortened_url_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ShortenedUrl, type: :model do
-  it "validates the presence of url" do
+  describe "validations" do
+    subject { ShortenedUrl.new(url: "http://www.google.co.uk/", unique_key: '24kbz') }
     
+    it { should validate_presence_of(:url).with_message('Please provide a URL') }
+  
+    it { should validate_uniqueness_of(:url).case_insensitive.with_message('Please choose another URL') }
+  
+    it { should validate_uniqueness_of(:unique_key) }
   end
   
   it "prepends http to the url" do
@@ -10,10 +16,8 @@ RSpec.describe ShortenedUrl, type: :model do
   end
   
   it "generates unique key of the given url" do
-      
   end
   
-  it "returns short form of the urll" do
-    
+  it "returns short form of the url" do
   end
 end
